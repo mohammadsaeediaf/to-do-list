050b2c2ecb1664817b608690f9a1148399eb4393
VanGuard <alisaadati18000@gmail.com>
2023-10-11 22:49:01 +0330

    Add Selector to script

---
+++ script.js
@@ -0,0 +1,8 @@
+// selector:
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todos-button");
+const todoListcontainer = document.querySelector(".todolist-container");
+//add EventListener
+
+
+
