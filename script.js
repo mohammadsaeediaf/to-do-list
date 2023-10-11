c8fd21f4066d43f54da4cf7be212de917879b31f
VanGuard <alisaadati18000@gmail.com>
2023-10-11 22:49:38 +0330

    Add eventlisener and write function

--- script.js
+++ script.js
@@ -5,4 +5,17 @@ const todoListcontainer = document.querySelector(".todolist-container");
 //add EventListener
 
 
+todoButton.addEventListener("click", addTodo);
 
+// functions
+function addTodo(e) {
+  e.preventDefault();
+  console.log(e);
+  const addTododiv = document.createElement("div");
+  addTododiv.classList.add("todo-list");
+  addTododiv.innerHTML = `
+  <li>Task 1</li>
+  <span> <i class="far fa-check-square"></i></span>
+  <span> <i class="far fa-trash-alt"></i></span>
+  `;
+}
